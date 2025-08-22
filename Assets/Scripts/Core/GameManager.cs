652695d339364d2ac795cc70766906c44284d2f1
konel <tomport@naver.com>
2025-08-23 06:46:58 +0900

    feat: CardSystems

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{ 
+        public static GameManager Instance { get; private set; }
+        public ManagerContainer Container{get; private set;}
+
+        public void Awake()
+        {
+                if (Instance != null && Instance != this)
+                { 
+                    Destroy(gameObject); 
+                }
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                
+                Container = new ManagerContainer();
+        }
+        
+}
---
+++ Assets/Scripts/Core/ManagerContainer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerContainer
+{
+private Dictionary<Type,object> managers = new Dictionary<Type, object>();
+
+public void Register<T>(T manager)
+{
+    managers.Add(typeof(T), manager);
+}
+
+public T GetManager<T>()
+{
+    if (managers.TryGetValue(typeof(T), out var manager))
+    {
+        return (T)manager;
+        
+    }
+    throw new Exception($"[ManagerContainer] {typeof(T)} 매니저가 등록되지 않았습니다.");
+}
+}
